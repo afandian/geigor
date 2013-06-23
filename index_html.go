b771726ed27bf0bdf8c07961a3d35c5046de7b92
joe@afandian.com <joe.wass@torchbox.com>
2013-06-23 18:57:06 +0100

    Style homepage.

--- index_html.go
+++ index_html.go
@@ -1,13 +1,27 @@
 package main
 
 var INDEX_HTML string = `<html>
-    <head>
+<head>
+        <link href='http://fonts.googleapis.com/css?family=Cagliostro' rel='stylesheet' type='text/css' />
         <title>Geigor</title>
-    </head>
-    <body>
+        <style type="text/css">
+            body {
+                font-family: 'Cagliostro', sans-serif;
+                margin: 30px;
+                padding: 0px;
+                color: 2b3a42;
+                background-color: #fff9f9;
+            }
+
+            a {
+                text-decoration: underline;
+                color: inherit;
+            }
+        </style>
+    </head>    <body>
         <h1>Geigor</h1>
-        <p>This is a geigor instance.</p>
-        <p>See <a href="http://afandian.com/geigor">http://afandian.com/geigor</a></p>
+        <p>This is a geigor instance. Welcome. </p>
+        <p>See <a href="http://afandian.com/geigor">http://afandian.com/geigor</a> for more information.</p>
 
         <ul>
             <li><a href="demo">Demo tracking page</a>.</li>
